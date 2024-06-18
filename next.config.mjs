f5ee4cf449eae7bc4069127fcac703816ab93464
Lifulifu <andycscsmaple@gmail.com>
2024-06-19 01:10:11 +0800

    dev branch should not have this next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/portfolio-nextjs",
-  output: "export",
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
